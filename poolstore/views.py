4542e189759d8d2c238635c2a7e260fc4834bb08
Luka <lgurjidze@gmail.com>
2024-06-13 19:15:25 +0400

    format available res. times

--- poolstore/views.py
+++ poolstore/views.py
@@ -184,8 +184,10 @@ class ReservationView(CreateView):
         
         start_time = datetime.combine(datetime.today().date(), time(10, 0, 0))
         close_time = datetime.combine(datetime.today().date(), time(3, 0, 0))
+        day_end_time = datetime.combine(datetime.today().date(), time(0, 0, 0))
 
         start_stamped = False
+        end_stamped = False
 
 
         for index in range(0, len(reservations)):
@@ -195,43 +197,60 @@ class ReservationView(CreateView):
             if current_reservation_start_datetime > start_time and not start_stamped:
                 start_stamped = True
 
-                if current_reservation_start_datetime - start_time > timedelta(minutes=30):
+                if current_reservation_start_datetime - start_time >= timedelta(minutes=30):
                     current_reservations.append(start_time)
                     next_reservations.append(current_reservation_start_datetime)
 
                     continue
 
 
-                    
+            if current_reservation_start_datetime < close_time and not end_stamped:
+                end_stamped = True
+
+                if current_reservation_start_datetime - day_end_time >= timedelta(minutes=30):
+                    current_reservations.append(day_end_time)
+                    next_reservations.append(current_reservation_start_datetime)
+
+                    continue
 
 
-            current_reservations.append(current_reservation)
             current_reservation_datetime = datetime.combine(current_reservation.date, current_reservation.real_end_time)
+            current_reservations.append(current_reservation_datetime)
             
                 
 
             try:
                 next_reservation = reservations[index + 1]
-                next_reservations.append(next_reservation)
                 next_reservation_datetime = datetime.combine(next_reservation.date, next_reservation.start_time)
+                next_reservations.append(next_reservation_datetime)
 
                 if next_reservation_datetime - current_reservation_datetime < timedelta(minutes=30):
-                    next_reservations.remove(next_reservation)
-                    current_reservations.remove(current_reservation)
+                    next_reservations.remove(next_reservation_datetime)
+                    current_reservations.remove(current_reservation_datetime)
 
-                if current_reservation_datetime < close_time and next_reservation_datetime > close_time \
-                    and close_time - current_reservation_datetime > timedelta(minutes=30):
-                    next_reservations.remove(next_reservation)
-                    next_reservations.append(close_time)
+                if current_reservation_datetime < close_time and next_reservation_datetime > close_time:
+                    next_reservations.remove(next_reservation_datetime)
+                    current_reservations.remove(current_reservation_datetime)
+                    if close_time - current_reservation_datetime >= timedelta(minutes=30):
+                        next_reservations.append(close_time)
+                        current_reservations.append(current_reservation_datetime)
 
 
                     
             except IndexError:
-                dt = datetime.combine(current_reservation.date + timedelta(days=1), time(0, 0, 0))
-                if dt - current_reservation_datetime > timedelta(minutes=30):
-                    next_reservations.append(dt)
+                if current_reservation_datetime > start_time:
+                    dt = datetime.combine(current_reservation.date + timedelta(days=1), time(0, 0, 0))
+                    if dt - current_reservation_datetime >= timedelta(minutes=30):
+                        next_reservations.append(dt)
+                        break
+                    current_reservations.remove(current_reservation_datetime)
+
                 else:
-                    current_reservations.remove(current_reservation)
+                    if not close_time - current_reservation_datetime >= timedelta(minutes=30):
+                        current_reservations.remove(current_reservation_datetime)
+                        break
+                    next_reservations.append(close_time)
+                    
 
 
                 
