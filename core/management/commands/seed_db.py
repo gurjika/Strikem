040862b667ef1ed87233b6aa7a24e43261e5ffa3
Luka <lgurjidze@gmail.com>
2024-10-26 22:22:13 +0400

    add seed file

--- core/management/commands/seed_db.py
+++ core/management/commands/seed_db.py
@@ -4,14 +4,26 @@ from pathlib import Path
 import os
 
 
+from django.core.management.base import BaseCommand
+from django.db import connection
+from pathlib import Path
+import os
+
 class Command(BaseCommand):
     help = 'Populates the database with collections and products'
 
     def handle(self, *args, **options):
         print('Populating the database...')
         current_dir = os.path.dirname(__file__)
-        file_path = os.path.join(current_dir, 'poolhub_dump1.sql')
+        file_path = os.path.join(current_dir, 'seed.sql')
         sql = Path(file_path).read_text()
 
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
+        try:
+            with connection.cursor() as cursor:
+                for statement in sql.split(';'):
+                    if statement.strip():
+                        cursor.execute(statement)
+        except Exception as e:
+            print("An error occurred:", e)
+        else:
+            print("Database populated successfully.")
