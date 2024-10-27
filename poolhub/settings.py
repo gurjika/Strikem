2fc6994293dbeb243091e6e75a0c9e4749fd95fa
Luka <lgurjidze@gmail.com>
2024-10-27 19:34:24 +0400

    update allowed hosts

--- poolhub/settings.py
+++ poolhub/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['134.122.88.48']
 
 
 # Application definition
