d0586076eff375aa93bb032308c524fc0b6f7ecb
Luka <lgurjidze@gmail.com>
2024-11-04 17:14:55 +0400

    show lat and lng

--- poolstore_api/serializers.py
+++ poolstore_api/serializers.py
@@ -124,10 +124,11 @@ class PoolHouseImageSerializer(serializers.ModelSerializer):
 class PoolHouseSerializer(serializers.ModelSerializer):
     tables = PoolTableSerializer(many=True, read_only=True)
     avg_rating = serializers.FloatField(read_only=True)
-    latitude = serializers.FloatField(write_only=True)
-    longitude = serializers.FloatField(write_only=True)
+  
     pics = PoolHouseImageSerializer(read_only=True, many=True)
     table_count = serializers.IntegerField(read_only=True)
+
+    
     class Meta:
         model = PoolHouse
         fields = ['id', 'title', 'address', 'tables', 'avg_rating', 'latitude', 'longitude', 'pics', 'table_count']
