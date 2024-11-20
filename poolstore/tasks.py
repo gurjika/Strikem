eb72589c6854fa720147ec4d2789994d812d530f
Luka <lgurjidze@gmail.com>
2024-11-20 13:37:04 +0400

    configure celery beat

--- poolstore/tasks.py
+++ poolstore/tasks.py
@@ -1,5 +1,7 @@
+from datetime import timedelta
 from celery import shared_task
 from django.contrib.contenttypes.models import ContentType
+from django.utils.timezone import now
 
 from poolstore.models import GameSession, Invitation, Message, Notification, Player, InvitationDenied
 
@@ -23,3 +25,10 @@ def create_notification(player, sent_by, type, body=None, extra=None):
 def delete_denied_invite(id):
     invitation_denied = InvitationDenied.objects.get(id=id)
     invitation_denied.delete()
+
+
+
+@shared_task
+def delete_outdated_notifications():
+    cutoff_date = now() - timedelta(days=5)
+    Notification.objects.filter(timestamp__lt=cutoff_date).delete()
