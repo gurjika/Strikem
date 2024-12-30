0c28a1ba45552ce8533a85d825d6fc56d4286368
Luka <lgurjidze@gmail.com>
2024-12-30 22:20:00 +0400

    update reservation email

--- poolstore_api/tasks.py
+++ poolstore_api/tasks.py
@@ -126,12 +126,12 @@ def finish_game_session(game_session_id, reservation_id, protocol):
     
 
 @shared_task
-def send_email_before_res(user_id):
+def send_email_before_res(user_id, res_id):
     user = User.objects.get(id=user_id)
 
     send_mail(
         subject='Poolhub Reservation',
-        message='5 minutes before reservation',
+        message=f'5 minutes before reservation {res_id}',
         from_email='luka.gurjidze04@gmail.com',
         recipient_list=[user.email],
         fail_silently=False ## TESTING
