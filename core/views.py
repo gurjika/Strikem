61506af74950ffbaedb6bf9fbe8769cbcea97ea4
Luka <lgurjidze@gmail.com>
2025-01-21 20:11:57 +0400

    add google auth (v1)

--- core/views.py
+++ core/views.py
@@ -13,7 +13,10 @@ from poolstore_api.serializers import PlayerSerializer
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_decode
 from rest_framework import status
-
+from utils import GoogleRawLoginFlowService
+from django.shortcuts import redirect
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class MyLoginView(LoginView):
@@ -82,3 +85,93 @@ class CurrentUserView(APIView):
     def get(self, request):
         serializer = PlayerSerializer(request.user.player)
         return Response(serializer.data)
+    
+
+
+
+class GoogleLoginRedirectApi(View):
+    def get(self, request, *args, **kwargs):
+        google_login_flow = GoogleRawLoginFlowService()
+
+        authorization_url, state = google_login_flow.get_authorization_url()
+
+        request.session["google_oauth2_state"] = state
+
+        return redirect(authorization_url)
+
+
+
+class GoogleLoginApi(APIView):
+    class InputSerializer(serializers.Serializer):
+        code = serializers.CharField(required=False)
+        error = serializers.CharField(required=False)
+        state = serializers.CharField(required=False)
+
+    def get(self, request, *args, **kwargs):
+        input_serializer = self.InputSerializer(data=request.GET)
+        input_serializer.is_valid(raise_exception=True)
+
+        validated_data = input_serializer.validated_data
+
+        code = validated_data.get("code")
+        error = validated_data.get("error")
+        state = validated_data.get("state")
+
+        if error is not None:
+            return Response(
+                {"error": error},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if code is None or state is None:
+            return Response(
+                {"error": "Code and state are required."}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        session_state = request.session.get("google_oauth2_state")
+
+        if session_state is None:
+            return Response(
+                {"error": "CSRF check failed."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        del request.session["google_oauth2_state"]
+
+        if state != session_state:
+            return Response(
+                {"error": "CSRF check failed."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+
+
+        google_login_flow = GoogleRawLoginFlowService()
+
+        google_tokens = google_login_flow.get_tokens(code=code)
+
+        id_token_decoded = google_tokens.decode_id_token()
+        user_info = google_login_flow.get_user_info(google_tokens=google_tokens)
+
+        user_email = id_token_decoded["email"]
+        user = User.objects.filter(email=user_email)
+
+        if user is None:
+            return Response(
+                {"error": f"User with email {user_email} is not found."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+
+        result = {
+            "access_token": access_token,
+            "refresh_token": str(refresh),
+            "id_token_decoded": id_token_decoded,
+            "user_info": user_info,
+        }
+
+        return Response(result, status=status.HTTP_200_OK)
+
