f7447f818c1a90655b2c5dc5b6bc9cefc6e83bf8
Luka <lgurjidze@gmail.com>
2025-01-29 22:10:25 +0400

    update pagination

--- poolstore_api/pagination.py
+++ poolstore_api/pagination.py
@@ -19,6 +19,6 @@ class RatingPagination(PageNumberPagination):
 
 
 class FilterRatingPagination(PageNumberPagination):
-    page_size = 3
+    page_size = 15
     page_size_query_param = 'page_size'
     max_page_size = 100
