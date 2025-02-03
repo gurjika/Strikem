a731979e283de06c2a6e60f9d6bec30f305e01e3
Luka <lgurjidze@gmail.com>
2025-02-03 11:24:23 +0400

    update signal

--- poolstore/signals.py
+++ poolstore/signals.py
@@ -4,20 +4,6 @@ from django.dispatch import receiver
 from .models import Player, Notification, NotificationChoices
 
 
-# @receiver(post_save, sender=Matchup)
-# def remove_player_from_matchmake_list_on_accept(sender, instance, created, **kwargs):
-#     if created:
-#         channel_layer = get_channel_layer()
-#         group_name = 'matchmake'
-        
-#         event = {
-#             'type': 'accepting_player_cleanup',
-#             'accepter_username': instance.player_accepting.user.username,
-#             'inviter_username': instance.player_inviting.user.username,
-#         }
-
-#         async_to_sync(channel_layer.group_send)(group_name, event)
-
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -26,6 +12,6 @@ def create_player_for_new_user(sender, instance, created, **kwargs):
         if not instance.is_staff:
             player = Player.objects.create(user=instance, games_played=0, opponents_met=0, games_won=0)
 
-            if not instance.has_usable_password():
+            if not bool(instance.password):
                 Notification.objects.create(player=player, sent_by=None, body='Please set your account password to fully access its features', type=NotificationChoices.SET_PASSWORD)
 
